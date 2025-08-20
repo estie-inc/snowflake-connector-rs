88e22eba4396ed953806c469dcda62dc19675f30
yuly3 <yuly3masapon@gmail.com>
2025-08-20 15:09:20 +0900

    apply clippy

--- src/query.rs
+++ src/query.rs
@@ -91,11 +91,10 @@ impl QueryExecutor {
             return Err(Error::Communication(response.message.unwrap_or_default()));
         }
 
-        if let Some(format) = response.data.query_result_format {
-            if format != "json" {
+        if let Some(format) = response.data.query_result_format
+            && format != "json" {
                 return Err(Error::UnsupportedFormat(format.clone()));
             }
-        }
 
         let http = http.clone();
         let qrmk = response.data.qrmk.unwrap_or_default();
