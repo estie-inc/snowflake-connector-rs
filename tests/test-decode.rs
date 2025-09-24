37778b13b19bc79ca1e1727da0c3010883b5f3b5
kenkoooo <kenkou.n@gmail.com>
2025-09-24 22:23:25 +0900

    test: simplify SQL queries in decode test for improved readability

--- tests/test-decode.rs
+++ tests/test-decode.rs
@@ -10,17 +10,7 @@ async fn test_decode() -> Result<()> {
 
     // Create a temporary table
     // DATETIME aliases TIMESTAMP_NTZ
-    let query = "CREATE TEMPORARY TABLE example (
-        n NUMBER,
-        s STRING,
-        b BOOLEAN,
-        d DATE,
-        tm TIME,
-        ltz TIMESTAMP_LTZ,
-        ntz TIMESTAMP_NTZ,
-        tz TIMESTAMP_TZ,
-        u UUID,
-    )";
+    let query = "CREATE TEMPORARY TABLE example (n NUMBER, s STRING, b BOOLEAN, d DATE, tm TIME, ltz TIMESTAMP_LTZ, ntz TIMESTAMP_NTZ, tz TIMESTAMP_TZ, u UUID)";
     let rows = session.query(query).await?;
     assert_eq!(rows.len(), 1);
     assert_eq!(
@@ -29,17 +19,7 @@ async fn test_decode() -> Result<()> {
     );
 
     // Insert some data
-    let query = "INSERT INTO example (n, s, b, d, tm, ltz, ntz, tz, u) VALUES (
-        42,
-        'hello',
-        0,
-        '2024-01-01',
-        '01:23:45',
-        '2024-01-01 00:00:00',
-        '2024-01-01 00:00:00',
-        '2024-01-01 00:00:00',
-        '2af9c142-ca9d-4d7b-bf2c-ae4458bae85f'
-    )";
+    let query = "INSERT INTO example (n, s, b, d, tm, ltz, ntz, tz, u) VALUES (42,'hello',0,'2024-01-01','01:23:45','2024-01-01 00:00:00','2024-01-01 00:00:00','2024-01-01 00:00:00','2af9c142-ca9d-4d7b-bf2c-ae4458bae85f')";
     let rows = session.query(query).await?;
     assert_eq!(rows.len(), 1);
     assert_eq!(rows[0].get::<i64>("NUMBER OF ROWS INSERTED")?, 1);
