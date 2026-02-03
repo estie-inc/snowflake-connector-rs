31d7fce5ed3c67165b684e7e7440320328a58335
yuly3 <yuly3masapon@gmail.com>
2026-02-03 15:13:52 +0900

    feat: add external browser SSO example and feature flag configuration

--- src/auth/client.rs
+++ src/auth/client.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "external-browser-sso")]
 use std::time::Duration;
 
+#[cfg(feature = "external-browser-sso")]
 use serde_json::json;
 
 pub(super) fn client_app_id() -> &'static str {
@@ -10,6 +12,7 @@ pub(super) fn client_app_version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
 
+#[cfg(feature = "external-browser-sso")]
 pub(super) fn client_environment(timeout: Option<Duration>) -> serde_json::Value {
     json!({
         "OCSP_MODE": "FAIL_OPEN",
